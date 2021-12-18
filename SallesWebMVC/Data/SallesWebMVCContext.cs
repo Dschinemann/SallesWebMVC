f5a238efc82e2a164aabebed58d3c983381d0119
Diego Schinemann <diego.alexandre@hotmail.com>
2021-12-18 16:19:30 -0300

    First

---
+++ SallesWebMVC/Data/SallesWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SallesWebMVC.Models;
+
+namespace SallesWebMVC.Data
+{
+    public class SallesWebMVCContext : DbContext
+    {
+        public SallesWebMVCContext (DbContextOptions<SallesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+
+    }
+}
